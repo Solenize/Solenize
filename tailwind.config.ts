ed305499eaf6d043d9a07c6a76ddcde2a64283ef
ryan010417 <ryan010417@gmail.com>
2024-04-11 01:02:57 +0800

    01-Landing_Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        lightSecondary: '#D0E4E5',
+        primary: '#06F6EE',
+        placeholder: '#CBD5E1',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'my_bg_image' : "url('../public/landing.png')"
       },
     },
   },
